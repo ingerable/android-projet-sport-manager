97959d0173f7b54170d762636892b7e33c9db9c0
jules <j.vanassche@etu.unistra.fr>
2019-12-14 12:19:17 +0100

    floating button create training programm

--- app/src/main/java/com/example/sportmanager/Components/Fragments/TrainingProgram/TrainingProgramFragment.java
+++ app/src/main/java/com/example/sportmanager/Components/Fragments/TrainingProgram/TrainingProgramFragment.java
@@ -4,15 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.sportmanager.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class TrainingProgramFragment extends Fragment {
 
@@ -30,6 +34,18 @@ public class TrainingProgramFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        FloatingActionButton fab = root.findViewById(R.id.trainingProgram_fab_add);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+
         return root;
     }
 }
